4de3190f4fd165c24d8b65a63b301ef517775d1b
Albin Rdz <ealbinu@gmail.com>
2017-09-04 14:30:06 -0500

    Funcioning100%

--- app/app.js
+++ app/app.js
@@ -7,9 +7,45 @@ var app = new Vue({
     data: {
       activeTab: 0,
       sceneTab:0,
-      tickets: []
+      user: {
+        usuario: null,
+        contrasena: null,
+        adidas: 0,
+        reebok: 0
+      },
+      loadingComponent : null
     },
     methods: {
+      login : function(){
+        app.loadingComponent = this.$loading.open();
+        if(app.user.usuario == null || app.user.contrasena == null){
+          this.$dialog.alert('Ingresa los datos de acceso');
+          return false;
+        }
+        var credentials = {"type":"seller", "key": "usuario", "value":app.user.usuario };
+        fetch('https://dragonbarbudo.com/api/grabasa/r/', {
+          method: "POST",
+          body: JSON.stringify(credentials)
+        })
+        .then(function(u){ return u.json(); })
+        .then(function(json){
+          app.loadingComponent.close();
+          if(json!=null){
+            if(json['contrasena']==app.user.contrasena){
+              app.activeTab=3;
+              app.user = json;
+            } else {
+              app.$dialog.alert('Error en los datos de acceso');
+            }
+          } else {
+            app.$dialog.alert('Error en los datos de acceso');
+          }
+          
+        });
+
+
+        
+      }
     },
 
 })
